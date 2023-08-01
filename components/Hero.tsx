d79c0f6a3dd61a913f0619f6010d618fd9889a15
sapey <alexandre.froment92@gmail.com>
2023-08-02 00:10:24 +0200

    finish the hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,7 +22,14 @@ const Hero = () => {
             <CustomButton 
                 title="Explore car"
                 containerStyles="bg-primary-blue text-white rounded-full mt-10"
-                handleClick={handleScroll}/>
+                handleClick={handleScroll}
+            />
+        </div>
+        <div className='hero__image-container'>
+            <div className='hero__image'>
+                <Image src="/hero.png" alt='hero' fill className="object-contain" />
+                <div className='hero__image-overlay' />
+            </div>
         </div>
     </div>
   )
