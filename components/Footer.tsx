ae07cdd12429c8911949f62d86d1bc6eea23481c
sapey <alexandre.froment92@gmail.com>
2023-08-06 00:52:10 +0200

    finish the layouts and the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
+import { footerLinks } from '@/constants';
+
 const Footer = () => {
   return (
     <footer className='flex flex-col text-black-500 mt-5 border-t border-gray-100'>
@@ -17,6 +19,41 @@ const Footer = () => {
                     CarHub 2023 <br/>
                     All right reserved &copy;
                 </p>
+            </div>
+
+            <div className='footer__links'>
+                {footerLinks.map((link)=>(
+                    <div key={link.title} className='footer__link'>
+                        <h3 className='font-bold'>{link.title}</h3>
+                        {link.links.map((item)=>(
+                            <Link 
+                                key={item.title}
+                                href={item.url}
+                                className='text-gray-500'
+                            >
+                                {item.title}
+                            </Link>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </div>
+
+        <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+            <p>@2023 CarHub. All Rights Reserved</p>
+            <div className='footer__copyrights-link'>
+                <Link
+                    href="/"
+                    className='text-gray-500'
+                >
+                    Privacy Policy
+                </Link>
+                <Link
+                    href="/"
+                    className='text-gray-500'
+                >
+                    Term of Use
+                </Link>
             </div>
         </div>
     </footer>
