488e8d502d10e60baa17abf0e5936e88c053db46
sapey <alexandre.froment92@gmail.com>
2023-08-05 02:30:57 +0200

    start the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='flex flex-col text-black-500 mt-5 border-t border-gray-100'>
+        <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
+            <div className='flex flex-col justify-start items-start gap-6'>
+                <Image
+                    src="/logo.svg"
+                    alt='logo'
+                    width={118}
+                    height={18}
+                    className='object-contain'
+                />
+                <p className='text-base text-gray-700'>
+                    CarHub 2023 <br/>
+                    All right reserved &copy;
+                </p>
+            </div>
+        </div>
+    </footer>
   )
 }
 
